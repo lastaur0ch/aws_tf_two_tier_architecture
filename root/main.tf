83d34fd99d73b8473072c820f267263d1b98082c
lastaur0ch <joelxjohnson@gmail.com>
2024-03-19 12:29:57 +0100

    updated modules

--- root/main.tf
+++ root/main.tf
@@ -1,5 +1,30 @@
 module "vpc" {
   source = "../modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = var.vpc_cidr
   project_name = var.project_name
+  region = var.region
+  pub_sub_1a_cidr = var.pub_sub_1a_cidr
+  pub_sub_2b_cidr = var.pub_sub_2b_cidr
+  pri_sub_3a_cidr = var.pri_sub_3a_cidr
+  pri_sub_4b_cidr = var.pri_sub_4b_cidr
+  pri_sub_5a_cidr = var.pri_sub_5a_cidr
+  pri_sub_6b_cidr = var.pri_sub_6b_cidr
 }
+
+module "nat" {
+  source = "../modules/nat"
+  igw_id = module.vpc.igw_id
+  vpc_id = module.vpc.vpc_id
+  pri_sub_3a_id = module.vpc.pri_sub_3a_id
+  pri_sub_4b_id = module.vpc.pri_sub_4b_id
+  pri_sub_5a_id = module.vpc.pri_sub_5a_id
+  pri_sub_6b_id = module.vpc.pri_sub_6b_id
+  pub_sub_1a_id = module.vpc.pub_sub_1a_id
+  pub_sub_2b_id = module.vpc.pub_sub_2b_id
+}
+
+module "security-group" {
+  source = "../modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
+
